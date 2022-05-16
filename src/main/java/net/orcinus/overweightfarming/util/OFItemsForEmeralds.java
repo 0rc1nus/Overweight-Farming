7decce67833bfc819cbde4eb0ee6b99d55c850f2
0rc1nus <mitchellctlui@gmail.com>
2022-05-16 21:41:57 +0800

    More renaming

--- src/main/java/net/orcinus/overweightfarming/util/OFItemsForEmeralds.java
+++ src/main/java/net/orcinus/overweightfarming/util/OFItemsForEmeralds.java
@@ -18,26 +18,26 @@ public class OFItemsForEmeralds implements VillagerTrades.ItemListing {
     private final int villagerXp;
     private final float priceMultiplier;
 
-    public OFItemsForEmeralds(Item p_35741_, int p_35742_, int p_35743_, int p_35744_) {
-        this(new ItemStack(p_35741_), p_35742_, p_35743_, 12, p_35744_);
+    public OFItemsForEmeralds(Item item, int emeraldCost, int numberOfItems, int villagerXp) {
+        this(new ItemStack(item), emeraldCost, numberOfItems, 12, villagerXp);
     }
 
-    public OFItemsForEmeralds(ItemStack p_35752_, int p_35753_, int p_35754_, int p_35755_, int p_35756_) {
-        this(p_35752_, p_35753_, p_35754_, p_35755_, p_35756_, 0.05F);
+    public OFItemsForEmeralds(ItemStack stack, int emeraldCost, int numberOfItems, int maxUses, int villagerXp) {
+        this(stack, emeraldCost, numberOfItems, maxUses, villagerXp, 0.05F);
     }
 
-    public OFItemsForEmeralds(ItemStack p_35758_, int p_35759_, int p_35760_, int p_35761_, int p_35762_, float p_35763_) {
-        this.itemStack = p_35758_;
-        this.emeraldCost = p_35759_;
-        this.numberOfItems = p_35760_;
-        this.maxUses = p_35761_;
-        this.villagerXp = p_35762_;
-        this.priceMultiplier = p_35763_;
+    public OFItemsForEmeralds(ItemStack stack, int emeraldCost, int numberOfItems, int maxUses, int villagerXp, float priceMultiplier) {
+        this.itemStack = stack;
+        this.emeraldCost = emeraldCost;
+        this.numberOfItems = numberOfItems;
+        this.maxUses = maxUses;
+        this.villagerXp = villagerXp;
+        this.priceMultiplier = priceMultiplier;
     }
 
     @Nullable
     @Override
-    public MerchantOffer getOffer(Entity p_35706_, Random p_35707_) {
+    public MerchantOffer getOffer(Entity entity, Random random) {
         return new MerchantOffer(new ItemStack(Items.EMERALD, this.emeraldCost), new ItemStack(this.itemStack.getItem(), this.numberOfItems), this.maxUses, this.villagerXp, this.priceMultiplier);
     }
 }
