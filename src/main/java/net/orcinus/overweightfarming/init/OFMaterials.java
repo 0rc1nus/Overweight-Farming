ace3fe42608c185d9658cf49197fcd7fd39bb795
0rc1nus <mitchellctlui@gmail.com>
2022-06-01 18:43:30 +0800

    parse fix

---
+++ src/main/java/net/orcinus/overweightfarming/init/OFMaterials.java
@@ -0,0 +1,10 @@
+package net.orcinus.overweightfarming.init;
+
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
+
+public class OFMaterials {
+
+    public static final Material OVERWEIGHT_PLANT = (new Material.Builder(MaterialColor.PLANT)).noCollider().destroyOnPush().build();
+
+}
