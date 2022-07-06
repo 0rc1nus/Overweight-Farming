8c7c4888897686b01ba06c58c825d9b0aa164f2d
mrsterner <simon@sterner.dev>
2022-07-06 16:54:12 +0200

    fix #21

--- src/main/java/net/orcinus/overweightfarming/common/registry/OFMaterials.java
+++ src/main/java/net/orcinus/overweightfarming/common/registry/OFMaterials.java
@@ -4,7 +4,5 @@ import net.minecraft.block.MapColor;
 import net.minecraft.block.Material;
 
 public class OFMaterials {
-
-    public static final Material OVERWEIGHT_PLANT = (new Material.Builder(MapColor.WHITE_GRAY)).notSolid().replaceable().build();
-
+    public static final Material OVERWEIGHT_PLANT = (new Material.Builder(MapColor.WHITE_GRAY)).notSolid().build();
 }
