a92772cbf274e3a591d3fcde5b888ce6ad237838
mrsterner <simon@sterner.dev>
2022-07-24 14:32:02 +0200

    ops

--- src/main/java/net/orcinus/overweightfarming/mixin/compat/WeedsBlockMixin.java
+++ src/main/java/net/orcinus/overweightfarming/mixin/compat/WeedsBlockMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.CropBlock;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
+import net.orcinus.overweightfarming.OFConfig;
 import net.orcinus.overweightfarming.OverweightFarming;
 import net.orcinus.overweightfarming.common.blocks.OverweightWeedBlock;
 import net.orcinus.overweightfarming.common.registry.OFObjects;
@@ -24,7 +25,7 @@ public class WeedsBlockMixin extends CropBlock {
 
     @Inject(method = "randomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z"), cancellable = true)
     private void OF$randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci){
-        if (OverweightFarming.config.compatCrops.allowOverweightWeeds && state.contains(CropBlock.AGE) && state.get(CropBlock.AGE) == 3) {
+        if (OFConfig.allowOverweightWeeds && state.contains(CropBlock.AGE) && state.get(CropBlock.AGE) == 3) {
             if (random.nextFloat() < 0.005) {
                 if(world.getBlockState(pos.up(3)).isAir() && world.getRandom().nextBoolean()){
                     OverweightWeedBlock.placeTallAt(world, OFObjects.OVERWEIGHT_WEED.getDefaultState(), pos, Block.NOTIFY_LISTENERS);
