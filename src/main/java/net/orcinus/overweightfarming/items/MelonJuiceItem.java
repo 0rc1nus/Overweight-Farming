112a814175a6059f50cb1cdda6b2c6b7f19f65f5
QinomeD <cpueater@interia.pl>
2023-08-31 08:30:27 +0200

    forgot about that whole bottle return and eating sound and anim stuff

--- src/main/java/net/orcinus/overweightfarming/items/MelonJuiceItem.java
+++ src/main/java/net/orcinus/overweightfarming/items/MelonJuiceItem.java
@@ -1,18 +1,49 @@
 package net.orcinus.overweightfarming.items;
 
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 
 public class MelonJuiceItem extends Item {
+
     public MelonJuiceItem(Properties properties) {
         super(properties);
     }
 
     @Override
     public ItemStack finishUsingItem(ItemStack stack, Level world, LivingEntity entity) {
+        super.finishUsingItem(stack, world, entity);
         entity.heal(3);
-        return super.finishUsingItem(stack, world, entity);
+        if (stack.isEmpty()) {
+            return new ItemStack(Items.GLASS_BOTTLE);
+        } else {
+            if (entity instanceof Player player && !player.getAbilities().instabuild) {
+                ItemStack bottle = new ItemStack(Items.GLASS_BOTTLE);
+                if (!player.getInventory().add(bottle))
+                    player.drop(bottle, false);
+            }
+            return stack;
+        }
+    }
+
+    @Override
+    public UseAnim getUseAnimation(ItemStack stack) {
+        return UseAnim.DRINK;
+    }
+
+    @Override
+    public SoundEvent getDrinkingSound() {
+        return SoundEvents.HONEY_DRINK;
+    }
+
+    @Override
+    public SoundEvent getEatingSound() {
+        return SoundEvents.HONEY_DRINK;
     }
 }
